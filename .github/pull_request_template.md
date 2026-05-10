698028c3a108f449635d4829b71f385247e38a12
JP Madrigal <jp.madrigal07@gmail.com>
2026-05-10 18:55:40 +0800

    Add semantic release configuration and changelog setup. Introduce GitHub Actions for automated releases, and update package dependencies for semantic release plugins. Create changelog configuration for structured release notes.

---
+++ .github/pull_request_template.md
@@ -0,0 +1,20 @@
+## REMINDER OF THE REQUIRED ACTIONS
+
+- [ ] No console message/errors/warning thrown to the logs
+- [ ] Have run `pnpm run build` and no failing builds shown
+- [ ] Commit messages was squashed to a single commit [Tutorial](https://www.youtube.com/watch?v=DLadleLj0ic)
+- [ ] Reviewers were tagged in the PR sidebar
+- [ ] Assignees was tagged in the PR sidebar
+- [ ] Labels was added in the PR sidebar
+- [ ] Project was added in the PR sidebar
+- [ ] Milestone was added in the PR sidebar
+
+## INSERT PR DESCRIPTION BELOW
+
+### Comments
+
+\_Provide elaboration or other comments that you need to note (optional)
+
+### Screenshots or Video
+
+\_Provide screenshots or short video to show that it is working (optional)
---
+++ .releaserc.js
@@ -0,0 +1,52 @@
+const change = require('./changelog.config');
+
+module.exports = {
+  branches: [
+    'main',
+    {
+      name: 'staging',
+      prerelease: true,
+    },
+  ],
+  repositoryUrl: 'https://github.com/jpmadrigal07/jeichat',
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+        releaseRules: [
+          { type: 'refactor', release: 'patch' },
+          { type: 'chore', release: 'patch' },
+        ],
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+        presetConfig: change,
+      },
+    ],
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
+  ],
+};
